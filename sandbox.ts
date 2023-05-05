77b9e56e71907d062e883b45a9afe367c5687157
Fachrul Arifin <fhrlarfn@gmail.com>
2023-05-05 23:15:04 +0700

    #3 type basic and declare parameter type on function

--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,22 @@
-const character = "mario";
-console.log(character);
+// To compile "TS (Typescript)" file, use command "tsc [filename]" on terminal
+// we can add -w to auto compile every save
+// you can change the value but not the type
+let character = "mario";
+let age = 30;
+let isBlackBelt = true;
 
-const inputs = document.querySelectorAll("input");
-console.log(inputs);
+// character = 30
+character = "luigi";
 
-inputs.forEach((input) => {
-  console.log(input);
-});
+// age = 'bowser'
+age = 20;
+
+// isBlackBelt = 'yes'
+isBlackBelt = false;
+
+//you can declare parameter type on function
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+};
+
+console.log(circ(7.5));
