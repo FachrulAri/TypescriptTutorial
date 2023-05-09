b42674bb509547a85bc2893a230ee6f186c653e8
Fachrul Arifin <fhrlarfn@gmail.com>
2023-05-09 23:05:03 +0700

    #7 Better workflow & tsconfig

---
+++ public/sandbox.js
@@ -0,0 +1,5 @@
+"use strict";
+// To compile "TS (Typescript)" file, use command "tsc [filename]" on terminal
+// we can add -w to auto compile every save
+// to initiate tsc config type "tsc --init" on terminal
+console.log("test");
---
+++ src/sandbox.ts
@@ -0,0 +1,5 @@
+// To compile "TS (Typescript)" file, use command "tsc [filename]" on terminal
+// we can add -w to auto compile every save
+// to initiate tsc config type "tsc --init" on terminal, use "tsc" to compile all ts files
+
+console.log("test");
