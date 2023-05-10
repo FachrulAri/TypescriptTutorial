4c114ec31ed0467eee9e7cba9e595f2aa7e19428
Fachrul Arifin <fhrlarfn@gmail.com>
2023-05-10 23:16:20 +0700

    #8 function basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,5 +1,19 @@
 "use strict";
 // To compile "TS (Typescript)" file, use command "tsc [filename]" on terminal
 // we can add -w to auto compile every save
-// to initiate tsc config type "tsc --init" on terminal
-console.log("test");
+// to initiate tsc config type "tsc --init" on terminal, use "tsc" to compile all ts files
+let greet;
+greet = () => {
+    console.log("hello world");
+};
+// if u want to add optional parameter, add "?" after parameter name
+// example "c?: number"
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10);
+const minus = (a, b) => {
+    return a - b;
+};
+let result = minus(10, 7);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -2,4 +2,22 @@
 // we can add -w to auto compile every save
 // to initiate tsc config type "tsc --init" on terminal, use "tsc" to compile all ts files
 
-console.log("test");
+let greet: Function;
+greet = () => {
+  console.log("hello world");
+};
+
+// if u want to add optional parameter, add "?" after parameter name
+// example "c?: number"
+const add = (a: number, b: number, c: number | string = 10) => {
+  console.log(a + b);
+  console.log(c);
+};
+
+add(5, 10);
+
+const minus = (a: number, b: number) => {
+  return a - b;
+};
+
+let result = minus(10, 7);
